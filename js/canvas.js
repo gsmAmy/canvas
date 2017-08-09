1ff5a3ff0c9e8a012054863f707eb89ba766881a
gsm <1746457008@qq.com>
2017-08-09 23:50:36 +0800

    canvas

--- js/canvas.js
+++ js/canvas.js
@@ -43,10 +43,24 @@ if(drawing.getContext){
 	//阴影
 	context.shadowOffsetX = 5;
 	context.shadowOffsetY = 5;
-	context.shadowBlur = 2;
+	context.shadowBlur = 3;
 	context.shadowColor = 'rgba(0,0,0,0.5)';
 	context.fillStyle = 'ff0000';
 	context.fillRect(100,100,100,100);
+
+	//线性渐变
+	var gradient = context.createLinearGradient(320,320,400,400);
+	gradient.addColorStop(0,'white');
+	gradient.addColorStop(1,'black');
+	context.fillStyle = gradient;
+	context.fillRect(320,320,100,100);
+
+	//放射渐变
+	var gradientRa = context.createRadialGradient(370,150,10,370,150,30);
+	gradientRa.addColorStop(0,'white');
+	gradientRa.addColorStop(1,'black');
+	context.fillStyle = gradientRa;
+	context.fillRect(320,100,100,100);
 }
 
 
